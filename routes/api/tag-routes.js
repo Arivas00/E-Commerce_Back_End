86efd5ef267113b38d3b3fa8796d10fe19a9810f
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-03-28 21:49:59 -0400

    Fixed get routes for products and tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag}]
+      include: [{ model: Product, through: ProductTag, as: 'products'}]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk({
-      include: [{ model: Product, through: ProductTag}]
+      include: [{ model: Product, through: ProductTag, as: 'products'}]
     });
 
     if (!tagData) {
